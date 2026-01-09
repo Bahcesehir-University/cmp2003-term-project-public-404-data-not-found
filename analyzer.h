5a1a15b87c208984e1ec23c544b04bcd1fc1758c
Demirkan Çevik <demirkancevik48@gmail.com>
2026-01-09 23:04:00 +0300

    h

--- analyzer.h
+++ analyzer.h
@@ -20,12 +20,14 @@ class TripAnalyzer {
 
 private:
     unordered_map<string, int64_t> zone_counts;
-    unordered_map<string, unordered_map<int, int64_t>> slot_counts;
+    unordered_map<string, unordered_map<int,int64_t>> slot_counts;
 
     bool parseCSV(const string& line, string&  zoneId, int& hour) const;
 
 public:
     void ingestFile(const string& csvPath);
-    vector<ZoneCount> topZones(int k = 10) const;
-    vector<SlotCount> topBusySlots(int k = 10) const;
+    vector<ZoneCount> topZones(int k =10) const;
+    vector<SlotCount> topBusySlots(int k=10) const;
 };
+
+
